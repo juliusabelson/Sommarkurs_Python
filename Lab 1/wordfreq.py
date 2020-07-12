ad6c60eaecbbce9aa2bb5e0dec11edb74f761259
juliusabelson <julius.abelson@gmail.com>
2020-07-12 13:44:56 +0200

    Tokenize working properly

--- Lab 1/wordfreq.py
+++ Lab 1/wordfreq.py
@@ -3,32 +3,42 @@ def tokenize(lines):
     for line in lines:
         start = 0
         while start < len(line):
-
-            while line[start].isspace():
-                start += 1
-
-                if start == len(line):
-                    return words
             
-
+            if line[start].isspace():
+                while line[start].isspace():
+                    start += 1
+                    if start == len(line):
+                        return words
+            
             end = start
             if line[end].isalpha():
                 while line[end].isalpha():
                     end += 1
-                
-                words.append(line[start:end].lower())
-                    
-                
+                    if end == len(line):
+                        break
+
+
+                word = line[start:end].lower()
+                words.append(word)
+                end = end - 1
+
             elif line[end].isdigit():
                 while line[end].isdigit():
                     end += 1
+                    if end == len(line):
+                        break
+
+                word = line[start:end]
+                words.append(word)
+                end = end - 1
                 
-                words.append(line[start:end].lower())
-            
+
             else:
-                words.append(line[start])
+                word = line[end]
+                words.append(word)
+            
+            start = end + 1
     return words
 
 
-tokenize(['sweet  apple  tarts.'])
 
